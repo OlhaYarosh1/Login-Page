8dc1ea8508ecd77a115cf43d06c85bb546edb833
Olha Yarosh <yarosholha1.gmail.com>
2022-07-08 12:51:54 +0300

    Added component, password input.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Form from './components/Form';
 import Footer from './components/Footer';
+import FooterForm from './components/FooterForm';
 
 function App() {
   return (
     <div className='container'>
       <Form />
+      <FooterForm/>
       <Footer />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -6,22 +6,17 @@ const Form = () => {
               <input className='emailInputTop' type='text'></input>
             </div>
             <input className='emailInputBottom' type='text'></input>
-          <div className='blueBorder' tabIndex='0'></div>  
-          <div className='placeholder'>Email address</div>   
+            <div className='blueBorder' tabIndex='0'></div>  
+            <div className='placeholder'>Email address</div>   
         </div>
         <div className='form-control'> 
           <div className='grayBorderPass' tabIndex='0'>
             <input className='passwordInputTop' type='password' />
           </div>
-            <input className='passwordInputBottom' type='text'></input>
-            <div className='placeholderPass'>Password</div>
-        </div>
-        <div className='form-control form-control-check'>
-            <input type='checkbox' />
-            <label>Remember me</label>
-            <a href='/'>Forgot password?</a>
-        </div>
-        <input type='submit' value='SIGN IN' className='btn btn-block'/>
+          <input className='passwordInputBottom' type='password'></input>
+          <div className='blueBorderPass' tabIndex='0'></div>  
+          <div className='placeholderPass'>Password</div>
+        </div>        
     </form>
   )
 }
